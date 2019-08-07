31fc115b4c927c0111dde14bc1933f62c625bf34
YochayCO <y.c@gmail.com>
2019-08-07 13:46:53 +0300

    Copy from Yaniv, fix pipe.groovy

--- basics/jenkins/job-dsl/pipe.groovy
+++ basics/jenkins/job-dsl/pipe.groovy
@@ -1,10 +1,19 @@
 pipelineJob('boilerplate-pipeline') {
+    def repo = 'https://github.com/YochayCO/docker-cicd.git'
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+
     definition {
         cpsScm {
             cps {
-                git('https://github.com/YochayCO/docker-cicd.git')
-                script(readFileFromWorkspace('./basics/misc/Jenkinsfile'))
-                sandbox()
+                remote { url('https://github.com/YochayCO/docker-cicd.git') }
+                branches('master')
+                scriptPath('misc/Jenkinsfile')
+                extensions {}
+                // git('https://github.com/YochayCO/docker-cicd.git')
+                // script(readFileFromWorkspace('./basics/misc/Jenkinsfile'))
             }
         }
     }
