6d4ee3e5cf120c2fd9713f5290afc386dde04385
YochayCO <y.c@gmail.com>
2019-08-07 13:56:25 +0300

    Get in the Git!

--- basics/jenkins/job-dsl/pipe.groovy
+++ basics/jenkins/job-dsl/pipe.groovy
@@ -8,12 +8,14 @@ pipelineJob('boilerplate-pipeline') {
     definition {
         cpsScm {
             cps {
-                remote { url('https://github.com/YochayCO/docker-cicd.git') }
-                branches('master')
-                scriptPath('misc/Jenkinsfile')
-                extensions {}
-                // git('https://github.com/YochayCO/docker-cicd.git')
-                // script(readFileFromWorkspace('./basics/misc/Jenkinsfile'))
+                git {
+                    remote { url(repo) }
+                    branches('master')
+                    scriptPath('misc/Jenkinsfile')
+                    extensions {}
+                    // git('https://github.com/YochayCO/docker-cicd.git')
+                    // script(readFileFromWorkspace('./basics/misc/Jenkinsfile'))
+                }
             }
         }
     }
